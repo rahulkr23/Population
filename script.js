11357c96a87c0714a37b73608a9bcfba60a8b7ea
rahulkr23 <rahulwhy23@gmail.com>
2021-04-01 00:53:19 +0530

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <div id="container"></div>
-    <script>
+  
         const container = document.querySelector('#container');
         const request = new XMLHttpRequest();
         request.open('GET', "https://restcountries.eu/rest/v2/name/nepal");
@@ -47,6 +36,4 @@
            `;
 
         })
-    </script>
-</body>
-</html>
+
